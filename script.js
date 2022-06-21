28b3c907b79517c0e6c8deffe2f096101748babb
jonny_g <jonathangormley23@gmail.com>
2022-06-21 18:02:18 +1000

    Initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+var string = "12";
+var number = 12;
+
+if (string === number) {
+  console.log("This is true, the values and types are the same.");
+} else if (string == number) {
+  console.log("This is also true for the same variables.");
+} else {
+  console.log("This does not ring true.");
+}
